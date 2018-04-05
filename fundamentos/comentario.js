32ed8c2834ddc4865b56a459a59bf2e6b6572cdb
Rodrigo Teruel <rodrigo.teruel@hotmail.com>
2018-04-05 15:21:13 -0300

    first commit

---
+++ fundamentos/comentario.js
@@ -0,0 +1,17 @@
+//comentario de uma linha
+console.log('linha 1')
+
+/*
+comentário de 
+múltiplas linhas
+*/
+console.log('linha 2')
+
+/*
+* comentário de 
+* múltiplas linhas
+*/
+console.log('linha 3')
+
+
+
