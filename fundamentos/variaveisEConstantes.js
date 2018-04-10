f67725745ffe044a50b78fac57a71a481e72f553
Rodrigo Teruel <rodrigo.teruel@hotmail.com>
2018-04-10 15:26:35 -0300

    atualização 10/04/2018 15:26

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,16 @@
+var a = 3
+let b = 4
+//let não pode ser declarado novamente, mas var pode
+var a = 30
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+// não pode atribuir outro valor após declarado
+const c = 5
+
+console.log(c)
